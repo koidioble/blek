12c0777561cf8757ebc7fee57ad30fbbb7620d57
Koidio <koidioble@gmail.com>
2025-06-10 20:06:49 -0400

    Update web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3761963071"
+    serviceWorkerVersion: "3778885099"
   }
 });
